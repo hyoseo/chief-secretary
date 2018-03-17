45231053b85c6cbc71f9b174a253d2376b153bea
thousand9 <hyoseo1009@gmail.com>
2018-03-17 21:08:29 +0900

    promise 단일 구현

--- app.js
+++ app.js
@@ -1,3 +1,75 @@
 'use strict';
 
-console.log('Hello world');
+
+const my_promise = require('./my-promise');
+
+function sumUpTo2(number) {
+    return new my_promise.MyPromise((resolve, reject) => {
+        // 단순히 2초 뒤 number까지의 합을 계산하는 함수
+        setTimeout(() => {
+            let sum = 0;
+
+            for (let i = 1; i < number; ++i) {
+                sum += i;
+            }
+
+            resolve(sum);
+        }, 2000);
+    });
+}
+
+sumUpTo2(100).then(sum => { console.log(`sum : ${sum}`) })
+
+return
+
+function sumUpTo(number) {
+    return new Promise((resolve, reject) => {
+        let sum = 0;
+
+        for (let i = 1; i < number; ++i) {
+            sum += i;
+        }
+
+        resolve(sum);
+    });
+}
+
+sumUpTo(100).then(sum => { console.log(`sum : ${sum}`)})
+
+return 
+const { Builder, By, Key, until } = require('selenium-webdriver');
+
+(async function example() {
+    let driver = await new Builder().forBrowser('chrome').build();
+    try {
+        await driver.get('http://www.google.com/ncr');
+        driver.getTitle().then(v => console.log(v));
+
+        //await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+        //await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    } finally {
+        await driver.quit();
+    }
+})();
+return
+
+const chrome = require('selenium-webdriver/chrome');
+//const firefox = require('../firefox');
+
+const width = 1920;
+const height = 1080;
+
+let driver = new Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(
+    new chrome.Options().headless().windowSize({ width, height }))
+    .build();
+
+driver.get('http://www.google.com/ncr')
+    .then(_ =>
+        console.log(driver.getTitle()))
+        //driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN))
+    //.then(_ => driver.wait(until.titleIs('webdriver - Google Search'), 1000))
+    .then(
+    _ => driver.quit(),
+    e => driver.quit().then(() => { throw e; }));
---
+++ my-promise.js
@@ -0,0 +1,54 @@
+﻿// Promise의 상태가 필요하다.
+const PENDING = 0;
+const FULFILLED = 1;
+const REJECTED = 2;
+
+module.exports.MyPromise = function(fn) {
+    // 처음 생성되었을 때의 상태는 PENDING이다.
+    let _state = PENDING;
+
+    // 결과 값을 저장한다.
+    let _value = null;
+
+    function fulfill(result) {
+        _state = FULFILLED;
+        _value = result;
+        _handlers.forEach(handle);
+        _handlers = null;
+    }
+
+    function reject(error) {
+        _state = REJECTED;
+        _value = error;
+        _handlers.forEach(handle);
+        _handlers = null;
+    }
+
+    let _handlers = [];
+
+    this.then = function(onFulfilled, onRejected) {
+        handle({ onFulfilled: onFulfilled, onRejected: onRejected });
+    }
+
+    function handle(handler) {
+        if (_state == PENDING) {
+            _handlers.push(handler);
+        } else if (_state == FULFILLED) {
+            handler.onFulfilled(_value);
+        } else if (_state == REJECTED) {
+            handler.onRejected(_value);
+        }
+    }
+
+    function doResolve(fn, onFulfilled, onRejected) {
+    // promise를 이용하는 함수는 인자로 resolve와 reject를 받아들인다.
+    // 따라서 resolve와 reject를 건내준다.
+        fn(function (value) {
+            onFulfilled(value);
+        }, function (reason) {
+            onRejected(reason);
+        });
+    }
+
+    doResolve(fn, fulfill, reject);
+}
